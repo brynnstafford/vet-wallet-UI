22eefacf7f525e8bbbfad076d6f8eb9cf0b37258
13212 <brynnstafford@gmail.com>
2022-04-04 23:01:34 -0400

    new pages and router commit

--- vet-wallet-ui/src/App.js
+++ vet-wallet-ui/src/App.js
@@ -1,12 +1,27 @@
-import React from "react"
-import Setup from "./components/Setup"
+import React from "react";
+import Nav from "./components/Nav"
+import Home from "./components/Home";
+import Setup from "./components/Setup";
+
+import {
+  BrowserRouter as Router, 
+  Route, 
+  Routes,
+  Switch, 
+  Link, 
+  Redirect 
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-        <Setup />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Setup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/nav" element={<Nav />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ vet-wallet-ui/src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+    return(
+        <nav>
+            <h3>Nav</h3>
+            <u1>
+                <li>Home</li>
+                <li>Setup</li>
+            </u1>
+        </nav>
+    );
+}
+
+export default Nav;
