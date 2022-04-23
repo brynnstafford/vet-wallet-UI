bf65673c098902cc93e2e44070d70096379932b4
xavier1120 <xaviernaranjo1130@gmail.com>
2022-04-23 16:38:05 -0400

    send var set

--- vet-wallet-ui/src/pages/Send.js
+++ vet-wallet-ui/src/pages/Send.js
@@ -1,39 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 const axios = require('axios');
-const baseURL = 'http://3.16.156.31:3000/user/api/v1/';
+const baseURL = 'http://3.16.156.31:3000/fabric/api/v1/';
 
-const sendBtn = () => {
-    // submit address (in input box) to backend as param for ERC20 CC
-    axios({
-        method: 'post',
-        url: baseURL + 'RequestTransfer',
-        data: {
-            fabricUserName: 'minter',
-            to: 'chron',
-            value: 10
-        }
-    });
-    alert('Transaction complete!');
-    window.location.assign('./Home');
-}
+function Send() {
 
-function recents() {
-    // at startup in page
-    // create new box for each recent transaction in order from newest to oldest
-    // loop boxes for how many transaction addresses previosuly been used
-    // limit 10
-    // box contains address
-    // onClick() address will populate into the search box
-}
+    const [to, setTo] = useState("");
+    const [amount, setAmount] = useState("");
 
-
-
-
-
-export default class Send extends Component {
-
-    render() {
+    const sendBtn = () => {
+        // submit address (in input box) to backend as param for ERC20 CC
+        axios({
+            method: 'post',
+            url: baseURL + 'RequestTransfer',
+            data: {
+                fabricUserName: sessionStorage.getItem('fabricUserName'),
+                to: to,
+                value: amount
+            }
+        });
+        alert('Transaction complete!');
+        //window.location.assign('./Home');
+    }
 
         return (
             <div className="send-page">
@@ -44,12 +32,22 @@ export default class Send extends Component {
                 <button className="cancel-send-btn" onClick={() => window.location = './Home'}>Cancel</button>
 
                 <div className="recipient-info">
-                    <p className="paste-text">Enter username: </p>
+                    <p className="paste-text">Enter Who you want to send to!: </p>
                     <form className="search">
-                        <input type="text" className="address_search" id="address_search" /> <br></br>
+                        <input type="text" 
+                        className="address_search" 
+                        id="address_search" 
+                        required
+                        value={to}
+                        onChange={e => setTo(e.target.value)}/><br></br>
 
                         <p>Enter amount:</p> <br></br>
-                        <input type="text" className="amount" id="amount" /> <br></br>
+                        <input type="number" 
+                        className="amount" 
+                        id="amount" 
+                        required
+                        value={amount}
+                        onChange={e => setAmount(e.target.value)}/><br></br>
                     </form>
                     <button className="submit-send" onClick={sendBtn}>
                         Send →
@@ -63,5 +61,5 @@ export default class Send extends Component {
                 </div>
             </div>
         );
-    }
 }
+export default Send;
