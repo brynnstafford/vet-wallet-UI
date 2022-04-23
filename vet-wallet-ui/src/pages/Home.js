ee06e77a8225af027efe57759f4d8c66431ed505
xavier1120 <xaviernaranjo1130@gmail.com>
2022-04-23 16:46:49 -0400

    use fetch instead on home

--- vet-wallet-ui/src/pages/Home.js
+++ vet-wallet-ui/src/pages/Home.js
@@ -1,27 +1,25 @@
-import React, { useState } from "react";
-
-
-const axios = require('axios');
+import React, { useState, useEffect } from "react";
 const baseURL = 'http://3.16.156.31:3000/user/api/v1/';
 
-async function showTotalBalance(){
-    let payload = { fabricUserName: sessionStorage.getItem('fabricUserName')};
-    return await axios.post(baseURL + 'GetAccountBalance', payload)
-}
-
-
 function Home()  {
- 
-
-    const [valuepp, setValuepp] = useState();
-        (async () => {
-            let res = await showTotalBalance()
-            let data = res;
-            const value = JSON.stringify(data.data)
-            console.log(value);
-            setValuepp(value)
-          })()
-
+    const [value, setValue] = useState();
+    (async () => {
+        useEffect(()=>{
+        fetch(baseURL + 'GetAccountBalance', {
+            method: "POST",
+            body: JSON.stringify({
+                fabricUserName: sessionStorage.getItem('fabricUserName')
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        // Converting to JSON
+        .then(response => response.json())
+        // Setting balance value to response value
+        .then(json => setValue(json));
+            },[])
+      })()
         return (
             <div className="home-page">
                 <header className="home-header">
@@ -38,7 +36,7 @@ function Home()  {
                     <h2 className="balance-title">Balance:</h2>
                     <form className="balance-box">
                         {/* create function to call backend getTotal() */}
-                        <output><p className="balance-amt">{valuepp}</p></output>
+                        <output><p className="balance-amt">{value}</p></output>
                     </form>
                     <button className="send-btn" onClick={() => window.location = './Send'}>
                         Send →
