76c84e9283fc63dab5e091ce3fc22ba81f5eef67
xavier1120 <xaviernaranjo1130@gmail.com>
2022-04-23 15:45:47 -0400

    createwallet varibles fixed

--- vet-wallet-ui/src/pages/CreateWallet.js
+++ vet-wallet-ui/src/pages/CreateWallet.js
@@ -1,13 +1,75 @@
-import React from "react";
-import UserInfo from "../components/UserInfo"
+import React, { useState } from "react";
+import Logo from "../vet-wallet-guy.png"
 
-export default function CreateWallet() {
+const axios = require('axios');
+const baseURL = 'http://3.16.156.31:3000/user/api/v1/';
 
-    return(
-        <div>
-            <UserInfo 
 
-            />
-        </div >
+function CreateWallet() {
+
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const walletSub = () => {
+        // save input as variables
+    
+        // pass username and password params to API/backend
+        axios({
+            method: 'post',
+            url: baseURL + 'registerAndEnrollUser',
+            data: {
+                fabricUserName: name,
+                password: password
+            }
+        });
+        // backend does stuff and creates account
+    
+        // connect account and go to home page
+        alert('Wallet Creation Complete!');
+        window.location.assign('/');
+    }
+
+    return (
+        <div className="create-page">
+            <header className="app-header">
+                <img className="logo" src={Logo} alt="vet logo" />
+                <p className="app-title">Vet wallet</p>
+            </header>
+
+            <button className="backToSetup" onClick={() => window.location = '/'}>
+                Back
+            </button>
+
+            <h1 className="create-wallet-text">Create Your Wallet</h1>
+            <div className="connect-body">
+                <h1>Please type a <br></br>Username and Password!</h1>
+
+                <form className="username-password">
+                    Username:
+                    <input
+                        type="text"
+                        id="username"
+                        required
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    /> <br></br>
+
+                    <br></br>
+                    Password:
+                    <input className='password'
+                        type="text"
+                        id="pw"
+                        required
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <br></br>
+                </form><br></br>
+
+                <button className="enrollAndRegister" onClick={walletSub}>
+                    Submit
+                </button>
+            </div>
+        </div>
     )
 }
+export default CreateWallet;
